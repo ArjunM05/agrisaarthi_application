ed6abbb15e886832772a406c5b5504963a5259f4
Abhinav Chaitanya R <159161126+Abhinav302004@users.noreply.github.com>
2025-07-20 19:10:04 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,14 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-      // proxy: {
-      //   "/predict": {
-      //     target:
-      //       "https://agrosaarthi-backend-ws-18-8000.ml.iit-ropar.truefoundry.cloud",
-      //     changeOrigin: true,
-      //     rewrite: (path: string) => path.replace(/^\/predict/, "/predict"),
-      //     secure: false,
-      //   },
-      // },
+      proxy: {
+        "/predict": {
+          target:
+            "https://agrosaarthi-backend-ws-18-8000.ml.iit-ropar.truefoundry.cloud",
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/predict/, "/predict"),
+          secure: false,
+        },
+      },
   },
 });
